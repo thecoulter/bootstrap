d68cc353fa23155281df6a628793bc519c893a47
Kris Lamoureux <KrisPublicEmail@gmail.com>
2020-01-23 20:27:23 -0500

    Add SSH keys, change hostname, and restart

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+  echo "You must run this script as root"
+  exit 1
+fi
+
 DATE=$(date '+%Y%m%d')
 TMP_DIR=$(mktemp -d)
-SOURCE_FILE="/etc/apt/sources.list"
 GIT_LOC="https://github.com/krislamo/bootstrap.git"
 
-mv $SOURCE_FILE $SOURCE_FILE.$DATE
-sed '/deb cdrom/d' $SOURCE_FILE.$DATE > $SOURCE_FILE
+echo "Enter name server's new hostname:"
+read NEW_HOSTNAME
+
+cp /etc/apt/sources.list /etc/apt/sources.list.$DATE
+sed -i '/deb cdrom/d' /etc/apt/sources.list
 
 apt-get update -y
 apt-get upgrade -y
@@ -15,3 +22,13 @@ apt-get install git -y
 cd $TMP_DIR
 git clone $GIT_LOC
 cd bootstrap
+
+mkdir -p /root/.ssh/
+cp --update authorized_keys /root/.ssh/authorized_keys
+apt-get install openssh-server -y
+
+hostnamectl set-hostname $NEW_HOSTNAME
+cp /etc/hosts /etc/hosts.$DATE
+sed -i "s/test/$NEW_HOSTNAME/g" /etc/hosts
+read -p "Press [enter] to restart this machine"
+/sbin/shutdown -r now
